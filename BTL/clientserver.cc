72239bbd4da431646bf7061055d553c1f2baf39d
tictactoe <myoki.crystal@gmail.com>
2019-11-21 02:13:41 +0700

    done share client list

--- BTL/clientserver.cc
+++ BTL/clientserver.cc
@@ -92,7 +92,7 @@ Sockpeer::Sockpeer(int localPort, std::string remoteHost, int remotePort, bool i
             this->connected = false;
             return;
         }
-        // printf("%d\n", n);
+
         serverReply = std::string(charReply, n);
         printf("Sockpeer::Sockpeer remote: %s:%u\n", server->host().c_str(),server->port());
 
@@ -106,7 +106,7 @@ Sockpeer::Sockpeer(int localPort, std::string remoteHost, int remotePort, bool i
         google::protobuf::util::ParseDelimitedFromZeroCopyStream(&serverMessageType, &zstream, &clean_eof);
         clean_eof = true;
         google::protobuf::util::ParseDelimitedFromZeroCopyStream(&serverClientInfo, &zstream, &clean_eof);
-        // std::cout << string_to_hex(serverClientInfo.SerializeAsString()) << std::endl;
+
         // Blacklist self from peer ClientInfo list
         marker = serverClientInfo.remotehost() + ":";
         marker += std::to_string(this->localPort);
@@ -116,7 +116,6 @@ Sockpeer::Sockpeer(int localPort, std::string remoteHost, int remotePort, bool i
             // BTL::HostInfo peer = serverClientInfo.peer(i);
             std::string peerString = peer.host() + ":";
             peerString += std::to_string(peer.port());
-            // std::cout << peerString << "eee\n";
             if (this->lookup.find(peerString) == this->lookup.end()){
                 // add to this->peers and lookup map
                 BTL::HostInfo* a = this->peers->add_peer();
@@ -146,147 +145,185 @@ Sockpeer::Sockpeer(int localPort, std::string remoteHost, int remotePort, bool i
 void Sockpeer::run(){
     // Helper variable
     size_t n;
+    char output_buffer[1024];
+    // Create 2 poll for stdin and socket
+    struct pollfd fds[2];
     if (this->isServer){
-        // List file here
+        // Server
 
-        char output_buffer[1024];
-        // Create 2 poll for stdin and socket
-        struct pollfd fds[2];
         /* Descriptor zero is stdin */
-        fds[0].fd = 0;
-        fds[1].fd = this->networkObj->recvfd;
+        fds[0].fd = this->networkObj->recvfd;
         fds[0].events = POLLIN | POLLPRI;
+
+        fds[1].fd = 0;
         fds[1].events = POLLIN | POLLPRI;
+    }
+    else {
+        // Client
+        fds[0].fd = this->networkObj->recvfd;
+        fds[0].events = POLLIN | POLLPRI;
+    }
+    // Normally we'd check an exit condition, but for this example we loop endlessly.
+    while (true) {
+        int ret = 0;
+        if (this->isServer){
+            ret = poll(fds, 2, -1);    
+        }
+        else {
+            ret = poll(fds, 1, -1);
+        }
+        
+        if (ret < 0) {
+            printf("Error - poll returned error: %s\n", strerror(errno));
+            break;
+        }
+        if (ret > 0) {
+            /* Regardless of requested events, poll() can always return these */
+            // if (fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) {
+            //     printf("Error - poll indicated socket error\n");
+            //     break;
+            // }
+            // if (fds[1].revents & (POLLERR | POLLHUP | POLLNVAL)) {
+            //     printf("Error - poll indicated stdin error\n");
+            //     break;
+            // }
 
-        // Normally we'd check an exit condition, but for this example we loop endlessly.
-        while (true) {
-            int ret = poll(fds, 2, -1);
+            /* Check if data to read from stdin */
+            if (this->isServer && fds[1].revents & (POLLIN | POLLPRI)) {
+                n = read(0, output_buffer, sizeof(output_buffer));
+                if (n < 0) {
+                    printf("Error - stdin error: %s\n", strerror(errno));
+                    break;
+                }
 
-            if (ret < 0) {
-                printf("Error - poll returned error: %s\n", strerror(errno));
-                break;
+                output_buffer[n] = '\x00';
+                // BTL::FileInfo requestedFile;
+                // requestedFile.set_filename(output_buffer);
+                std::cout << output_buffer << std::endl;
+                // Open file
+
+                // Calculate hash
+
             }
-            if (ret > 0) {
-                /* Regardless of requested events, poll() can always return these */
-                if (fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-                    printf("Error - poll indicated stdin error\n");
-                    break;
-                }
-                if (fds[1].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-                    printf("Error - poll indicated socket error\n");
-                    break;
-                }
 
-                /* Check if data to read from stdin */
-                if (fds[0].revents & (POLLIN | POLLPRI)) {
-                    n = read(0, output_buffer, sizeof(output_buffer));
-                    if (n < 0) {
-                        printf("Error - stdin error: %s\n", strerror(errno));
-                        break;
-                    }
-                    output_buffer[n] = '\x00';
-                    // BTL::FileInfo requestedFile;
-                    // requestedFile.set_filename(output_buffer);
-                    std::cout << output_buffer << std::endl;
-                    // Open file
+            /* Check if data to read from socket */
+            if (fds[0].revents & (POLLIN | POLLPRI)) {
+                std::cout << "wow, new packet\n";
+                // Recv packet
+                char charData[this->BUFFSIZE];
+                struct sockaddr_in peerAddress;
 
-                    // Calculate hash
-
-                    if (n < 0) {
-                        printf("Error - sendto error: %s\n", strerror(errno));
-                        break;
-                    }
+                n = this->networkObj->networkRecv(charData, this->BUFFSIZE, &peerAddress);
+                if (n == -1){
+                    continue;
                 }
 
-                /* Check if data to read from socket */
-                if (fds[1].revents & (POLLIN | POLLPRI)) {
-                    // Recv packet
-                    char charData[this->BUFFSIZE];
-                    std::string peerData;
-                    struct sockaddr_in peerAddress;
-                    n = this->networkObj->networkRecv(charData, this->BUFFSIZE, &peerAddress);
-                    if (n == -1){
-                        continue;
-                    }
-                    peerData = std::string(charData, n);
+                std::string peerData = std::string(charData, n);
+                std::string peerHost = inet_ntoa(peerAddress.sin_addr);
 
-                    // Read client info
-                    std::string peerHost = inet_ntoa(peerAddress.sin_addr);
-                    // std::cout << peerHost << std::endl;
-
-                    // Parse message
-                    BTL::MessageType peerMessageType;
-                    std::stringstream stream = std::stringstream(peerData);
-                    google::protobuf::io::IstreamInputStream zstream(&stream);
-                    bool clean_eof = true;
-                    google::protobuf::util::ParseDelimitedFromZeroCopyStream(&peerMessageType, &zstream, &clean_eof);
-                    clean_eof = true;
+                // Parse message
+                BTL::MessageType peerMessageType;
+                std::stringstream stream = std::stringstream(peerData);
+                google::protobuf::io::IstreamInputStream zstream(&stream);
+                bool clean_eof = true;
+                google::protobuf::util::ParseDelimitedFromZeroCopyStream(&peerMessageType, &zstream, &clean_eof);
+                clean_eof = true;
+                
+                if (peerMessageType.message() == BTL::MessageType::HOSTINFO) {
+                    // Some peer is updated to network, try to update if peer is not already
+                    BTL::HostInfo peer;
+                    google::protobuf::util::ParseDelimitedFromZeroCopyStream(&peer, &zstream, &clean_eof);
                     
-                    if (peerMessageType.message() == BTL::MessageType::HOSTINFO) {
-                        std::cout << "Sockpeer::run peer should not received HOSTINFO message\n";
-                        continue;
+                    // Add peer to network
+                    std::string peerString = peer.host() + ":";
+                    peerString += std::to_string(peer.port());
+                    if (this->lookup.find(peerString) == this->lookup.end()){
+                        // Peer not found, add new
+                        BTL::HostInfo* sender = this->peers->add_peer();
+                        sender->set_host(peer.host());
+                        sender->set_port(peer.port());
+                        sender->set_isserver(peer.isserver());
+                        this->lookup[peerString] = true;
                     }
-                    else if (peerMessageType.message() == BTL::MessageType::CLIENTINFO) {
-                        // ClientInfo message for exchange node
-                        BTL::ClientInfo peerClientInfo;
-                        google::protobuf::util::ParseDelimitedFromZeroCopyStream(&peerClientInfo, &zstream, &clean_eof);
-                        
-                        // Blacklist self from peer ClientInfo list
-                        std::string marker = peerClientInfo.remotehost() + ":";
-                        marker += std::to_string(this->localPort);
-                        this->lookup[marker] = true;
 
-                        // Add peer to network
-                        std::string peerString = peerHost + ":";
-                        peerString += std::to_string(peerClientInfo.localport());
-                        if (this->lookup.find(peerString) == this->lookup.end()){
-                            // key not found, add new
-                            BTL::HostInfo* sender = this->peers->add_peer();
-                            sender->set_host(peerHost);
-                            sender->set_port(peerClientInfo.localport());
-                            sender->set_isserver(false);
-                            this->lookup[peerString] = true;
-                        }
-                        
-                        for (auto peer : peerClientInfo.peer()){
-                            // BTL::HostInfo* peer = peerClientInfo.mutable_peer(i);
-                            peerString = peer.host() + ":";
-                            peerString += std::to_string(peer.port());
+                    // Show client list
+                    std::cout << "Sockpeer::run this client now contains " << this->peers->peer_size() << " peer(s)\n";
+                    for (auto i: this->peers->peer()){
+                        printf("%s:%d\n", i.host().c_str(), i.port());
+                    }
+                }
+                else if (peerMessageType.message() == BTL::MessageType::CLIENTINFO) {
+                    // ClientInfo message for exchange node
+                    BTL::ClientInfo peerClientInfo;
+                    google::protobuf::util::ParseDelimitedFromZeroCopyStream(&peerClientInfo, &zstream, &clean_eof);
+                    
+                    // Blacklist self from peer ClientInfo list
+                    std::string marker = peerClientInfo.remotehost() + ":";
+                    marker += std::to_string(this->localPort);
+                    this->lookup[marker] = true;
 
-                            if (this->lookup.find(peerString) == this->lookup.end()){
-                                // add to this->peers and lookup map
-                                BTL::HostInfo* a = this->peers->add_peer();
-                                a->set_host(peer.host());
-                                a->set_port(peer.port());
-                                a->set_isserver(peer.isserver());
-                                this->lookup[peerString] = true;
-                            }
-                        }
-                        // std::cout << "---------\n";
-                        // for (auto &x : this->lookup) {
-                        //     std::cout << x.first << std::endl;
-                        // }
-                        // std::cout << "---------\n";
-                        std::cout << "Sockpeer::run this client now contains " << this->peers->peer_size() << " peer(s)\n";
-                        for (auto i: this->peers->peer()){
-                            printf("%s:%d\n", i.host().c_str(), i.port());
-                        }
-                        
-                        // Return this->peers
+                    // Add peer to network
+                    std::string peerString = peerHost + ":";
+                    peerString += std::to_string(peerClientInfo.localport());
+                    if (this->lookup.find(peerString) == this->lookup.end()){
+                        // Peer not found, add new
+                        BTL::HostInfo* sender = this->peers->add_peer();
+                        sender->set_host(peerHost);
+                        sender->set_port(peerClientInfo.localport());
+                        sender->set_isserver(false);
+                        this->lookup[peerString] = true;
+
+                        // If new client update to network, send this->peers to the client
                         this->peers->set_remotehost(peerHost);
                         this->peers->set_localport(this->localPort);
                         std::string dataOut = wrapMessage(BTL::MessageType::CLIENTINFO, this->peers);
                         this->networkObj->networkSend(peerHost, peerClientInfo.localport(), dataOut);
+
+                        // Send peer HostInfo to all of my other client
+                        for (auto mypeer : this->peers->peer()){
+                            // skip the new updated client
+                            marker = mypeer.host() + ":";
+                            marker += std::to_string(mypeer.port());
+                            if ( marker == peerString ) {
+                                continue;
+                            }
+                            this->peers->set_remotehost(mypeer.host());
+                            this->peers->set_localport(this->localPort);
+                            std::string dataOut = wrapMessage(BTL::MessageType::HOSTINFO, sender);
+                            this->networkObj->networkSend(mypeer.host(), mypeer.port(), dataOut);
+                        }
                     }
-                    else {
-                        std::cout << "Command not found\n"; 
+                    
+                    // Add peer's client list to my client list
+                    for (auto peer : peerClientInfo.peer()){
+                        peerString = peer.host() + ":";
+                        peerString += std::to_string(peer.port());
+
+                        if (this->lookup.find(peerString) == this->lookup.end()){
+                            // Peer not found, add new
+                            BTL::HostInfo* a = this->peers->add_peer();
+                            a->set_host(peer.host());
+                            a->set_port(peer.port());
+                            a->set_isserver(peer.isserver());
+                            this->lookup[peerString] = true; 
+                        }
                     }
+                    // std::cout << "---------\n";
+                    // for (auto &x : this->lookup) {
+                    //     std::cout << x.first << std::endl;
+                    // }
+                    // std::cout << "---------\n";
+                    // Show client list
+                    std::cout << "Sockpeer::run this client now contains " << this->peers->peer_size() << " peer(s)\n";
+                    for (auto i: this->peers->peer()){
+                        printf("%s:%d\n", i.host().c_str(), i.port());
+                    }
+                }
+                else {
+                    std::cout << "Command not found\n";
                 }
             }
         }
-    }
-    else {
-        std::cout << "Prepare client\n"; 
     }
     return;
 };
