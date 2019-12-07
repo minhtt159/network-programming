0330fcdd10a4b6a8f2ba84b7c726b9ef46523bfd
tictactoe <myoki.crystal@gmail.com>
2019-12-08 03:13:54 +0700

    update folder for BT4

---
+++ BT4/Server/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ ! -f $(which protoc) ]]; then
+	echo "No Protobuf, run ../build_protobuf.sh"
+else
+	protoc --cpp_out=. udp.proto
+	if [ "$(uname)" == "Darwin" ]; then
+		g++ -Wall main.cpp clientserver.cc network.cc md5.cpp udp.pb.cc -std=c++1z -stdlib=libc++ -lprotobuf -o server       
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		g++ -Wall main.cpp clientserver.cc udp.pb.cc network.cc md5.cpp -pthread -I/usr/local/include -L/usr/local/lib -lprotobuf -static -fPIC -o server
+	else
+		echo "Platform is not supported"
+	fi
+fi
+
---
+++ BT4/Server/network.h
@@ -0,0 +1,33 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <errno.h>
+#include <ifaddrs.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Send UDP packet to peer
+size_t networkSend(std::string HOST, int PORT, std::string BUFFER);
+
+class Network
+{
+private:
+	// Buffer size for send & recv
+	socklen_t BUFFSIZE;
+public:
+	// Socket for receiving message
+	int recvfd;
+	// Recv UDP packet from peer, return condition, buffer & information about sender info
+	size_t networkRecv(char* BUFFER, size_t BUFFSIZE, sockaddr_in * CLIENT);
+	// Constructor create listening socket recvfd at PORT
+	Network(int port, int buffize);
+	// Destructor
+	~Network();
+};
+
+#endif /* !NETWORK_H */
